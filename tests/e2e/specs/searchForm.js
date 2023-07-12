abd38588aef1122f01ab9dfe0aea76913bb9196c
nascal3 <nascal3@gmail.com>
2023-07-12 20:43:18 +0300

    Correctly name test suite

--- tests/e2e/specs/searchForm.js
+++ tests/e2e/specs/searchForm.js
@@ -1,6 +1,6 @@
 // https://docs.cypress.io/api/table-of-contents
 
-describe('My First Test', () => {
+describe('Search page tests', () => {
   it('Visits the app root url', () => {
     cy.visit('/')
     cy.contains('h2', 'Cargo information')
